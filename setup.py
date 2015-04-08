ac40bac42f22d4a241af1c6e98c2e669b2de406d
Justin Kiggins <justin.kiggins@gmail.com>
2015-04-08 11:09:05 -0700

    inital commit (from glab-common-py)

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='klusta-pipeline',
+    version='0.0.1',
+    description='data pipeline for analyzing spike2 collected data in klustakwik ',
+    author='Justin Kiggins',
+    author_email='justin.kiggins@gmail.com',
+    packages=['klusta_pipeline',
+              ],
+    entry_points={
+        'console_scripts': [
+            's2mat_to_kwd = klusta_pipeline.s2mat_to_kwd:main',
+            'make_s2mat_list = kklusta_pipeline.make_s2mat_list:main'
+        ],
+    },
+)
