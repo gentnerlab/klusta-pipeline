6dffe3a10f1966f10d443eea4d52779e662fb377
Justin Kiggins <justin.kiggins@gmail.com>
2015-06-28 22:13:06 -0700

    refactored
    new poly3 template. export sitemap
    
    moved template dir
    
    refactored. prb files defined on the fly.
    
    tidying
    
    omit channels
    
    toward testing
    
    better RAM use.
    
    append to kwd, not write (and truncate)
    
    added destination arg

---
+++ klusta_pipeline/constants.py
@@ -0,0 +1,4 @@
+import os as _os
+
+MAX_CHANS = 256
+TEMPLATE_DIR = _os.path.join(_os.path.dirname(_os.path.realpath(__file__)),'templates')
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='klusta-pipeline',
-    version='0.0.1',
+    version='0.0.2',
     description='data pipeline for analyzing spike2 collected data in klustakwik ',
     author='Justin Kiggins',
     author_email='justin.kiggins@gmail.com',
@@ -12,8 +12,8 @@ setup(
               ],
     entry_points={
         'console_scripts': [
-            's2mat_to_kwd = klusta_pipeline.s2mat_to_kwd:main',
-            'make_s2mat_list = klusta_pipeline.make_s2mat_list:main'
+            'make_kwd = klusta_pipeline.make_kwd:main',
+            'display_probe = klusta_pipeline.probe:_display'
         ],
     },
 )
